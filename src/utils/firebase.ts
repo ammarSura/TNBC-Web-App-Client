c447d47d19c895c1ec63cc7e92b61e121ae8f32e
ammarSura <ammarsura@gmail.com>
2022-11-08 17:13:47 +0530

    feat: firebase-utils

---
+++ src/utils/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import request from 'axios'
+import firebaseConfig from '../firebase-config.json'
+
+export const signInWithGoogle = async() => {
+    const app = initializeApp(firebaseConfig)
+    const provider = new GoogleAuthProvider()
+    const auth = getAuth(app)
+    const result = await signInWithPopup(auth, provider)
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const idToken = await auth!.currentUser!.getIdToken()
+  
+    return idToken
+}
